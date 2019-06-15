e082ce563df6f946e6ea089253e3de35f7b76e80
Leonardo Vendrame <leonvendrame@gmail.com>
2019-06-15 11:17:04 -0300

    Quantização funcionando Método ainda muito lento

--- main.py
+++ main.py
@@ -7,9 +7,16 @@ import numpy as np
 import cv2 as ocv
 import random
 
-def minor_distance(image, sets):
-    print(None)
-    #calcular distancia
+def minor_distance(pixel, sets):
+    minor_distance_set = sets[0]
+    minor_distance = 9999999
+    for _set in sets:
+        distance = np.linalg.norm(pixel-_set)
+        if minor_distance > distance:
+            minor_distance = distance
+            minor_distance_set = _set
+
+    return minor_distance_set
 
 def main():
     color_number = sys.argv[1]
@@ -19,18 +26,16 @@ def main():
     # ocv.waitKey(0)
     # ocv.destroyAllWindows()
 
-    red = np.linspace(0, 255, 5)
+    red = np.linspace(0, 255, 3)
     green = np.linspace(0, 255, 3)
-    blue = np.linspace(0, 255, 2)
+    blue = np.linspace(0, 255, 3)
 
     sets = list(itertools.product(blue, green, red))
     sets = np.array(sets)
 
-    image[1:100:1,1:100:1] = sets[np.random.randint(0, 28)]
-    # for i in f:
-    #     print(i)
-
-    # image[:,:] = minor_distance
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            image[i][j] = minor_distance(image[i][j], sets)
 
     ocv.imshow("image", image)
     ocv.waitKey(0)
